400a18139e5f95abfc0d866d2ee958c84277d253
Jeff Eggink <jeffeggink@Jeffs-MacBook-Pro.local>
2018-07-28 13:28:50 -0700

    functionality

--- keys.js
+++ keys.js
@@ -2,10 +2,10 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: '3AnFPMqMWrMT5B8Va7cZDWGXR',
+  consumer_secret: 'FGlpUOq7Oy9ojw0jlQyB8nvj0y1kznPpyukz6YZP4WlBTuW7Mb',
+  access_token_key: '828433716640886784-1oBGa1JZ1WmtZ6vCvr3REg27i9KajvL',
+  access_token_secret: '6EWm9YOPP24jNgiaGba2ztNmmYB75D0fZqSJ48FEmZUd4',
 };
 
 exports.spotify = {
